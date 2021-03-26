1252a75e262d13d4fcc150d3a5f440527e0b6399
Vidoux <58913190+Vidoux@users.noreply.github.com>
2021-03-26 16:15:58 +0100

    mark chargers on the map

--- scripts/scipt.js
+++ scripts/scipt.js
@@ -1,8 +1,37 @@
-let map;
+// lien OpenChargeMap: https://openchargemap.org/site/develop/api
+// lien OpenStreetMap : https://wiki.openstreetmap.org/wiki/OpenLayers_Simple_Example
+let map = new OpenLayers.Map("demoMap");
+map.addLayer(new OpenLayers.Layer.OSM());
+map.zoomToMaxExtent();
+let markers = new OpenLayers.Layer.Markers( "Markers" );
+map.addLayer(markers);
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+const keyOpenChargeMap = "&key=256012ac-f9ca-4458-8ecb-4d6a4ffe4396";
+
+//Requête pour récupérer 9000 points de charge max en France uniquement
+let resURL = "https://api.openchargemap.io/v3/poi/?output=json&countrycode=FR&maxresults=9000&compact=true&verbose=false" + keyOpenChargeMap;
+
+fetch(resURL)
+.then(res => res.json())
+.then(data => data.forEach( function(element, index) {
+
+	markChargers(element.AddressInfo.Longitude, element.AddressInfo.Latitude)
+	})
+	)
+.catch(error => console.log(error));
+
+
+
+
+function markChargers(longitude, latitude){
+	console.log("latitude" + latitude);
+	console.log("longitude" + longitude);
+	var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+	var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+	var position       = new OpenLayers.LonLat(longitude, latitude).transform( fromProjection, toProjection);
+
+
+
+	markers.addMarker(new OpenLayers.Marker(position));
+
 }
